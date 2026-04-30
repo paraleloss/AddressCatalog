3e49e0e044e8e4dea0b4da5437455b79bdf01bd0
Saúl Pérez <saul.riot@gmail.com>
2026-04-29 19:58:53 -0600

    Create models

---
+++ AddressesCatalog/Models.swift
@@ -0,0 +1,34 @@
+//
+//  Models.swift
+//  AddressesCatalog
+//
+//  Created by Saúl Pérez on 29/04/26.
+//
+
+import Foundation
+
+struct Address: Codable, Identifiable {
+    let id: Int                    // AddressID
+    var addressLine1: String
+    var addressLine2: String?      // NULL
+    var city: String
+    var stateProvince: String
+    var countryRegion: String
+    var postalCode: String
+    var rowguid: String
+    var modifiedDate: Date
+    
+    var idString: String { "\(id)" }
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "AddressID"
+        case addressLine1 = "AddressLine1"
+        case addressLine2 = "AddressLine2"
+        case city = "City"
+        case stateProvince = "StateProvince"
+        case countryRegion = "CountryRegion"
+        case postalCode = "PostalCode"
+        case rowguid
+        case modifiedDate = "ModifiedDate"
+    }
+}
